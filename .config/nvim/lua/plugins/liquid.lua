05552ae630b2c2910da1ec2296368b736e4fe5f1
Devlin McClure <devlincodes@pm.me>
2024-05-10 10:03:42 -0400

    Add liquid treesitter parser

--- .config/nvim/lua/plugins/liquid.lua
+++ .config/nvim/lua/plugins/liquid.lua
@@ -6,4 +6,12 @@ return {
       table.insert(opts.ensure_installed, "curlylint")
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "liquid" })
+      end
+    end,
+  },
 }
